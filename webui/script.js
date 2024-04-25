2bd16f8240911e9072b9ab2b5b20f189ec3a7f02
jp1014506 <121205145+jp1014506@users.noreply.github.com>
2024-04-25 08:21:28 -0400

    Update script.js

--- webui/script.js
+++ webui/script.js
@@ -1,37 +1,42 @@
-var mysql = require('mysql');
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
-var http = require('http');
-var fs = require('fs');
 var bodyParser = require('body-parser');
 
+// MySQL connection setup
 var connection = mysql.createConnection({
-    host: 'mysql',
-    user: 'bbots',
-    password: 'csc468g6',
-    database: 'bargainBot',
-    port: 3306,
+    host: 'mysql',  // Kubernetes service name for MySQL
+    user: 'bbots',  // Your MySQL user
+    password: 'csc468g6',  // Your MySQL password
+    database: 'bargainBot',  // Your MySQL database
+    port: 3306
 });
 
 connection.connect(function(err) {
     if (err) throw err;
+    console.log('Connected to MySQL Server!');
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 var server = app.listen(80, function(){
-    console.log('webui running on port 80');
+    console.log('Web UI running on port 80');
 });
 
 app.get('/', function(req, res) {
-    res.sendFile(process.cwd() + '/files/');
+    res.sendFile(process.cwd() + '/files/index.html');  // Ensure this path is correct for your setup
 });
 
-app.get('/searched', function(req, res) {
-    res.sendFile(process.cwd() + '/files/');
+app.get('/api/products', (req, res) => {
+    connection.query('SELECT * FROM products', (error, results) => {
+        if (error) throw error;
+        res.json(results);
+    });
 });
 
 app.post('/sterm', (req, res) => {
-    connection.query('INSERT INTO search VALUE("' + req.body.search + '");');
+    const search = connection.escape(req.body.search);
+    connection.query(`INSERT INTO search VALUE(${search});`);
     res.redirect('/searched');
 });
